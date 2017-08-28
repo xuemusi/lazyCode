485e1396deb2fbdd0bd464c9ca0ffbfd01e321a9
xuemusi <694366306@qq.com>
2017-08-28 11:35:26 +0800

    配置文件

---
+++ Core/config/db.example.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xumusi
+ * Date: 2017/8/28
+ * Time: 11:25
+ */
+return [
+    'DB_TYPE' => 'MysqlPdo',
+    'DB_HOST' => '127.0.0.1',
+    'DB_USER' => 'homestead',
+    'DB_PWD' => 'secret',
+    'DB_PORT' => 3306,
+    'DB_NAME' => 'dev_db_et',
+    'DB_CHARSET' => 'utf8mb4',
+    'DB_PREFIX' => 'dux_',
+];
